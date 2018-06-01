4ae89df440f1a6de01765d2c859bd8ddc014d887
Josh Corneille <jcorneille107@gmail.com>
2018-06-01 09:07:02 -0400

    stuff

--- bsearch.py
+++ bsearch.py
@@ -1,8 +1,11 @@
 def bsearch(a, e):
     m = len(a) // 2
     if(e == a[m]):
+        print("middle: %d" % m)
         return m
     elif(e < a[m]):
+        print(m)
         bsearch(a[:m], e)
     else:
+        print(m)
         bsearch(a[m:], e)
--- bsearchTest.py
+++ bsearchTest.py
@@ -1,9 +1,29 @@
 import bsearch as bs
 
-# Test empty array
+a = [0, 1, 2]
+
+def printArray(array):
+    for el in array:
+        print(el, end=' ')
+    print()
+
+printArray(a)
+
 try:
-    empa = [0,1,1]
-    bs.bsearch(empa, 0)
+    print("Answer 0: ", end='')
+    print(bs.bsearch(a, 0))
+except Exception as e:
+    print(e)
+
+try:
+    print("Answer 1: ", end='')
+    print(bs.bsearch(a, 1))
+except Exception as e:
+    print(e)
+
+try:
+    print("Answer 2: ", end='')
+    print(bs.bsearch(a, 2))
 except Exception as e:
     print(e)
 
