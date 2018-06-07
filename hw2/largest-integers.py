cdef1bb3295164f75aa85582dd6030a2e7cf95d5
Josh Corneille <jcorneille107@gmail.com>
2018-06-07 01:20:25 -0400

    hw2

--- hw2/largest-integers.py
+++ hw2/largest-integers.py
@@ -1,77 +1,53 @@
-"""
-Find the largest integers in an array.
-Parameters:
-    a - array of integers
-    k - number of largest integers to find
-Precondition: k <= len(a)
-Return: an array containing the k largest elements of array a
-Running Time: O(nlog(k))
+def newheap(n):
+  return [0]*(n+1)
 
-Method:
-min-heap
-1) Insert k elements into the min-heap
-    For each element of the array
-        if element is < min element of min-heap
-            toss the element
-        else if element > min element of min-heap
-            pop min element from min-heap
-            insert element from array into min-heap
-    return all elements of the min-heap
+def insert(a,e):
+  a[0] = a[0] + 1
+  a[a[0]] = e
+  heapfixup(a,a[0])
 
-The min heap will contain k elements, all of which will be the largest.
-"""
+def heapfixup(a,i):
+  while i > 1:
+    p = i//2
+    if a[p] > a[i]:
+      a[p], a[i] = a[i], a[p]
+      i = p
+    else:
+      return -1
+
+def heapfixdown(a,i):
+  while 2*i <= a[0]:
+    c=2*i 
+    if c+1 <= a[0]:
+      if a[c+1] < a[c]:
+        c = c+1
+    if a[i] > a[c]:
+      a[i],a[c] = a[c],a[i]
+      i = c
+    else:
+      return
+
+def extractsmallest(a):
+  e,a[1],a[0] = a[1], a[a[0]], a[0]-1
+  heapfixdown(a,1)
+  return e
+
 def lgInts(a, k):
-    if(k <= len(a)):
+    # Create the min heap with k 0 elements
+    n = len(a)
+    minheap = newheap(k)
 
-"""
-Class min-heap
-Attributes:
-    n - size of heap
-methods:
-    init(n)
-        Creates array within object containing all 0s at length n
-    peek()
-"""
-class MinHeap(object):
-    def __init__(self, n):
-        self.a = [0]*(n)
-        self.size = n
+    # create min heap with the first k elements of a
+    for i in range(k):
+        insert(minheap, a[i])
 
-    # Inserts into the last available spot and then checks and fixes heap structure
-    def insert(e):
-        self.size += 1
-        self.a[self.size] = e
-        self.heapfixup()
-    
-    def extractsmallest(a):
-        e = self.a[0]
-        self.a[0] = self.a[self.size]
-        self.size =- 1
-        heapfixdown()
-        return e
-    
-    def heapfixup(i):
-        while i > 1:
-            p = i // 2
+    for j in range(k, n):
+        if a[j] > minheap[1]:
+            extractsmallest(minheap)
+            insert(minheap, a[j])
 
-            if self.a[p] > self.a[i]:
-                self.a[p], self.a[i] = self.a[i], self.a[p]
-                i = p
-            else:
-                return
-    
-    def heapfixdown(i):
-        while (2 * i) <= self.size:
-            c = 2 * i
-            if (c + 1) <= self.size:
-                if a[c] < a[c - 1]:
-                    c += 1
-                if a[i] > a[c - 1]:
-                    a[i], a[c - 1] = a[c - 1], a[i]
-                    i = c - 1
-                else:
-                    return
-    
+    return minheap[1:]
 
 
-    
+array = [0, 1, 3, 6, 2, 9, 8]
+print(lgInts(array, 3))
--- hw2/lgIntsTest.py
+++ hw2/lgIntsTest.py
@@ -1,5 +1,18 @@
+import largestIntegers as LI
 """
 Tests for largest integers
 """
-def test(n, k):
-    
+def printArray(array):
+    print("# ", end=' ')
+    for el in array:
+        print(el, end=' ')
+    print(": ", end=' ')
+
+def find(a, k):
+    largest = LI.lgInts(a, k)
+    for i in (len(largest)):
+        print("%d " % largest[i])
+
+
+
+find(genSeqArray(5), 2)
