c106e3a02544ad4bc62a0d8bdc8b3721c467d55b
Josh Corneille <jcorneille107@gmail.com>
2018-06-09 11:59:38 -0400

    graph alt

---
+++ graphs/graph-alt.py
@@ -0,0 +1,7 @@
+"""
+Graph uses dictionary with nodes as sequential integers and edges as pairs.
+"""
+graph = { 1: [1,2],
+        2: [2, 1],
+        3: [3, 1]
+        }  
--- graphs/graph.py
+++ graphs/graph.py
@@ -1,11 +1,12 @@
 """
-size - nunmber of vertices in graph
+size - number of vertices in graph
 g.vertices[i] - List of all vertices conntected to node i in graph g
 """
 class Graph:
     def __init__(self, size):
         self.size = size
         self.vertices = [[] for i in range(size)]
+    
     def add_edge(self,i,j):
         if not (i in self.vertices[j]):
             # adds vertex i to the list of vertices connected to j in graph g
